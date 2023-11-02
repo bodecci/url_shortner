499bea7da54c53f9c027da400ae01d9b1a70777c
Bode Falade <bodmas@bodes-mbp-2.mynetworksettings.com>
2023-11-01 20:53:11 -0500

    added URL redirect to long URL.

--- main.go
+++ main.go
@@ -13,4 +13,18 @@ var (
 	base62Charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 )
 
-func redirectToLongURL(w http.ResponseWriter, r *http)
+func redirectToLongURL(w http.ResponseWriter, r *http.Request) {
+	short := r.URL.Path[1:] // remove the leading slash from the URL
+
+	//check is the chort URL exists in the map
+	urlLock.RLock()
+	longURL, exists := shortURLS[short]
+	urlLock.Unlock()
+
+	if exists {
+		http.Redirect(w, r, longURL, http.StatusMovedPermanently) // redirect to long URL
+	}
+	if !exists {
+		http.NotFound(w, r)
+	}
+}
